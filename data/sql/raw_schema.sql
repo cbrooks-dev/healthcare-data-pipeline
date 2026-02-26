4f0d7bb4ecd226d5d3bd9240cd9a4330bea3b9a3
cbrooks-dev <connorbrooks04@yahoo.com>
2026-02-25 21:01:41 -0500

    Create flexible raw schema for data ingestion

---
+++ data/sql/raw_schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Patient (
+    name TEXT,
+    age INT,
+    gender TEXT,
+    blood_type TEXT,
+    medical_condition TEXT,
+    date_of_admission DATE,
+    doctor TEXT,
+    hospital TEXT,
+    insurance_provider TEXT,
+    billing_amount DECIMAL,
+    room_number INT,
+    admission_type TEXT,
+    discharge_date DATE,
+    medication TEXT,
+    test_results TEXT
+);
---
+++ pipeline/extract.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import sqlalchemy
+
+engine = sqlalchemy.create_engine()
